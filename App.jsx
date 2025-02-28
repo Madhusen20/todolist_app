99b296791fe6b30d0269a3a6921ed65c4b7b38e3
Madhusen20 <166596208+Madhusen20@users.noreply.github.com>
2025-02-28 22:13:05 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Todolist from './Todolist';
+
+function App() {
+   return (
+    <>
+  <div className='container'>
+ <Todolist/>
+  </div>
+  </>
+  )
+}
+
+export default App
